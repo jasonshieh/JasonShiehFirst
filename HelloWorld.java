ba88d44ac5c5014b2d5703618fa9ff3c0c7360d5
jasonshieh <jasonshieh.vip@gmail.com>
2012-06-27 14:37:42 +0800

    another change of log content..

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 
 	public static void main(String args[]){
 		System.out.println("May it be a good start of git manage");
+		System.out.println("But I'm not sure how to add partners here...");
 	}
 }
